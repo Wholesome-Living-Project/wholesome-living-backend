9d305652f726ed5626744feba60b083ef15b16eb
Pieli <49155289+Pieli@users.noreply.github.com>
2023-03-31 00:22:17 +0200

    Fix: missed during merge conflict resolution

--- internal/meditation/router.go
+++ internal/meditation/router.go
@@ -9,10 +9,5 @@ func Routes(app *fiber.App, controller *Controller) {
 
 	// add routes here
 	meditation.Post("/", controller.create)
-<<<<<<< HEAD
 	meditation.Get("/:meditationID", controller.get)
-=======
-	meditation.Get("/", controller.get)
-	meditation.Get("/", controller.get)
->>>>>>> e842873335bfa1f980665bba05fec1bfa34b774b
 }
