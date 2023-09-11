36051b48ea281be1490996c635186ee20cc8265b
Pieli <49155289+Pieli@users.noreply.github.com>
2023-09-11 19:54:41 +0200

    Wrote Meditation Test

--- internal/meditation/controller.go
+++ internal/meditation/controller.go
@@ -4,9 +4,10 @@ import (
 	"cmd/http/main.go/internal/progress"
 	"cmd/http/main.go/internal/settings"
 	"cmd/http/main.go/internal/user"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"strconv"
 )
 
 type Controller struct {
@@ -23,7 +24,7 @@ func NewController(storage *Storage, userStorage *user.Storage, progressStorage 
 	}
 }
 
-type createMeditationRequest struct {
+type CreateMeditationRequest struct {
 	MeditationTime int   `json:"meditationTime" bson:"meditationTime"`
 	EndTime        int64 `json:"endTime" bson:"endTime"`
 }
@@ -50,7 +51,7 @@ type getAllMeditationResponse []struct {
 // @Router /meditation [post]
 func (t *Controller) create(c *fiber.Ctx) error {
 	c.Request().Header.Set("Content-Type", "application/json")
-	var req createMeditationRequest
+	var req CreateMeditationRequest
 
 	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -136,8 +137,7 @@ func (t *Controller) get(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Missing userId header",
 		})
-	}
-	if userId != "" {
+	} else {
 		_, err := t.userStorage.Get(userId, c.Context())
 
 		if err != nil {
@@ -146,6 +146,7 @@ func (t *Controller) get(c *fiber.Ctx) error {
 				"err":     err,
 			})
 		}
+
 		// all meditations for a user between a time range and duration
 		meditations, err := t.storage.GetAllOfOneUserBetweenTimeAndDuration(userId, times, c.Context())
 		if err != nil {
@@ -154,18 +155,9 @@ func (t *Controller) get(c *fiber.Ctx) error {
 				"err":     err,
 			})
 		}
-		if len(meditations) != 0 {
-			//check if user is allowed to get this meditation
-			if meditations[0].UserID != userId {
-				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-					"message": "User is not allowed to get this meditation",
-				})
-			}
-			return c.Status(fiber.StatusOK).JSON(meditations)
-		}
 
+		return c.Status(fiber.StatusOK).JSON(meditations)
 	}
-	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Query constraints do not yield any results"})
 }
 
 func convertToInt64(value string) int64 {
